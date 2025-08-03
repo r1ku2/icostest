d9d1e02b0e853dac30e61d19a4ca355a3e91f124
Nitta <nitta@Nittas-MacBook-Pro.local>
2025-08-03 22:44:58 +0900

    test-env

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask
 import os
+import ibm_boto3
+from ibm_botocore.client import Config, ClientError
 
 app = Flask(__name__)
 
+def create_text_file(bucket_name, item_name, file_text):
+    print("Creating new item: {0}".format(item_name))
+    try:
+        cos_client.put_object(
+            Bucket=bucket_name,
+            Key=item_name,
+            Body=file_text
+        )
+        print("Item: {0} created!".format(item_name))
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to create text file: {0}".format(e))
+
 @app.route("/")
 def hello():
-    name = os.getenv("TARGET", "World")
+    name = os.getenv("TEST", "World")
     return f"Hello from Code Engine, {name}!\n"
 
 if __name__ == "__main__":
